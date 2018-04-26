f124d290491d79f2058b93388a2b81bede87b451
Evan Knowles <evank@discovery.co.za>
2018-04-26 10:02:27 +0200

    Clean up the code a little, use some newer Java features, make JetBrains / IntelliJ slightly less sad.

--- starter-bots/java/src/main/java/za/co/entelect/challenge/entities/GameDetails.java
+++ starter-bots/java/src/main/java/za/co/entelect/challenge/entities/GameDetails.java
@@ -5,7 +5,6 @@ import za.co.entelect.challenge.enums.BuildingType;
 import java.util.HashMap;
 
 public class GameDetails {
-
     public int round;
     public int mapWidth;
     public int mapHeight;
--- starter-bots/java/src/main/java/za/co/entelect/challenge/entities/Missile.java
+++ starter-bots/java/src/main/java/za/co/entelect/challenge/entities/Missile.java
@@ -1,10 +1,6 @@
 package za.co.entelect.challenge.entities;
 
-import za.co.entelect.challenge.enums.Direction;
-import za.co.entelect.challenge.enums.PlayerType;
-
 public class Missile extends Cell {
-
     private int damage;
     private int speed;
 }
