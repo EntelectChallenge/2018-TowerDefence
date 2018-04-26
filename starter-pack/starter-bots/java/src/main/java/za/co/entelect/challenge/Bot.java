3ae955916c26e91fe13de1d935d36dd64e2cfa70
Daan Keun <daan.keun@entelect.co.za>
2018-04-26 10:59:02 +0200

    Updated java starter bot in starter-pack with PR changes

--- starter-pack/starter-bots/java/src/main/java/za/co/entelect/challenge/Bot.java
+++ starter-pack/starter-bots/java/src/main/java/za/co/entelect/challenge/Bot.java
@@ -4,7 +4,6 @@ import za.co.entelect.challenge.entities.*;
 import za.co.entelect.challenge.enums.BuildingType;
 import za.co.entelect.challenge.enums.PlayerType;
 
-import java.time.LocalTime;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
@@ -14,7 +13,6 @@ import java.util.stream.Collectors;
 import static za.co.entelect.challenge.enums.BuildingType.*;
 
 public class Bot {
-
     private GameState gameState;
     private GameDetails gameDetails;
     private int gameWidth;
@@ -24,11 +22,14 @@ public class Bot {
     private List<Building> buildings;
     private List<Missile> missiles;
 
+    /**
+     * Constructor
+     * @param gameState the game state
+     **/
     public Bot(GameState gameState) {
         this.gameState = gameState;
         gameState.getGameMap();
         gameDetails = gameState.getGameDetails();
-//        energyPerTurn = gameDetails.;
         gameWidth = gameDetails.mapWidth;
         gameHeight = gameDetails.mapHeight;
         myself = gameState.getPlayers().stream().filter(p -> p.playerType == PlayerType.A).findFirst().get();
@@ -44,7 +45,11 @@ public class Bot {
 
     }
 
-    public String Run() {
+    /**
+     * Run
+     * @return the result
+     **/
+    public String run() {
         if (isUnderAttack()) {
             return defendRow();
         } else if (hasEnoughEnergyForMostExpensiveBuilding()) {
@@ -54,30 +59,44 @@ public class Bot {
         }
     }
 
-
+    /**
+     * Build random building
+     * @return the result
+     **/
     private String buildRandom() {
         List<CellStateContainer> emptyCells = gameState.getGameMap().stream()
                 .filter(c -> c.getBuildings().size() == 0 && c.x < (gameWidth / 2))
                 .collect(Collectors.toList());
-        if (emptyCells.size() == 0) {
+
+        if (emptyCells.isEmpty()) {
             return doNothingCommand();
         }
+
         CellStateContainer randomEmptyCell = getRandomElementOfList(emptyCells);
         List<BuildingType> buildingTypes = new ArrayList<>(gameDetails.buildingPrices.keySet());
         BuildingType randomBuildingType = getRandomElementOfList(buildingTypes);
+
         if (!canAffordBuilding(randomBuildingType)) {
             return doNothingCommand();
         }
+
         return buildCommand(randomEmptyCell.x, randomEmptyCell.y, randomBuildingType);
     }
 
+    /**
+     * Has enough energy for most expensive building
+     * @return the result
+     **/
     private boolean hasEnoughEnergyForMostExpensiveBuilding() {
-        boolean hasEnoughEnergy = gameDetails.buildingPrices.values().stream()
+        return gameDetails.buildingPrices.values().stream()
                 .filter(bp -> bp < myself.energy)
                 .toArray().length == 3;
-        return hasEnoughEnergy;
     }
 
+    /**
+     * Defend row
+     * @return the result
+     **/
     private String defendRow() {
         for (int i = 0; i < gameHeight; i++) {
             int opponentAttacksCount = getAllBuildingsForPlayer(PlayerType.B, b -> b.buildingType == ATTACK, i).size();
@@ -89,6 +108,10 @@ public class Bot {
         return buildRandom();
     }
 
+    /**
+     * Checks if this is under attack
+     * @return true if this is under attack
+     **/
     private boolean isUnderAttack() {
         //if enemy has an attack building and i dont have a blocking wall
         for (int i = 0; i < gameHeight; i++) {
@@ -101,27 +124,51 @@ public class Bot {
         return false;
     }
 
+    /**
+     * Do nothing command
+     * @return the result
+     **/
     private String doNothingCommand() {
         return "";
     }
 
+    /**
+     * Place building in row
+     * @param buildingType the building type
+     * @param y the y
+     * @return the result
+     **/
     private String placeBuildingInRow(BuildingType buildingType, int y) {
         List<CellStateContainer> emptyCells = gameState.getGameMap().stream()
                 .filter(c -> c.getBuildings().size() == 0
                         && c.y == y
                         && c.x < (gameWidth / 2) - 1)
                 .collect(Collectors.toList());
-        if (emptyCells.size() == 0) {
+
+        if (emptyCells.isEmpty()) {
             return buildRandom();
         }
+
         CellStateContainer randomEmptyCell = getRandomElementOfList(emptyCells);
         return buildCommand(randomEmptyCell.x, randomEmptyCell.y, buildingType);
     }
 
+    /**
+     * Get random element of list
+     * @param list the list < t >
+     * @return the result
+     **/
     private <T> T getRandomElementOfList(List<T> list) {
         return list.get((new Random()).nextInt(list.size()));
     }
 
+    /**
+     * Build command
+     * @param x the x
+     * @param y the y
+     * @param buildingType the building type
+     * @return the result
+     **/
     private String buildCommand(int x, int y, BuildingType buildingType) {
         StringBuilder buildCommand = new StringBuilder();
 
@@ -130,7 +177,6 @@ public class Bot {
         buildCommand.append(y);
         buildCommand.append(",");
         switch (buildingType) {
-
             case DEFENSE:
                 buildCommand.append("0");
                 break;
@@ -144,6 +190,13 @@ public class Bot {
         return buildCommand.toString();
     }
 
+    /**
+     * Get all buildings for player
+     * @param playerType the player type
+     * @param filter the filter
+     * @param y the y
+     * @return the result
+     **/
     private List<Building> getAllBuildingsForPlayer(PlayerType playerType, Predicate<Building> filter, int y) {
         return buildings.stream()
                 .filter(b -> b.getPlayerType() == playerType
@@ -152,6 +205,11 @@ public class Bot {
                 .collect(Collectors.toList());
     }
 
+    /**
+     * Can afford building
+     * @param buildingType the building type
+     * @return the result
+     **/
     private boolean canAffordBuilding(BuildingType buildingType) {
         int cost = 0;
         switch (buildingType) {
@@ -168,5 +226,4 @@ public class Bot {
 
         return myself.energy >= cost;
     }
-
 }
