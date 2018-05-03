7b23d3ae345f0f353b2bf1cf88ac71b6e775959d
edwin.fullard <edwin.fullard@entelect.co.za>
2018-05-03 19:24:09 +0200

    moved the bot meta data from config.json to bot.json

--- game-runner/src/main/java/za/co/entelect/challenge/bootstrapper/Config.java
+++ game-runner/src/main/java/za/co/entelect/challenge/bootstrapper/Config.java
@@ -1,18 +1,15 @@
 package za.co.entelect.challenge.bootstrapper;
-
-
 import com.google.gson.annotations.SerializedName;
-import za.co.entelect.challenge.entities.BotMetaData;
 
 public class Config {
-    @SerializedName("bot-meta")
-    public BotMetaData[] botMetaData;
+    @SerializedName("player-a")
+    public String playerAConfig;
 
-    @SerializedName("console-players")
-    public int consolePlayers;
+    @SerializedName("player-b")
+    public String playerBConfig;
 
     @SerializedName("max-runtime-ms")
-    public int maximumBotRuntimeMiliSeconds;
+    public int maximumBotRuntimeMilliSeconds;
 
     @SerializedName("round-state-output-location")
     public String roundStateOutputLocation;
--- game-runner/src/main/java/za/co/entelect/challenge/entities/BotMetaData.java
+++ game-runner/src/main/java/za/co/entelect/challenge/entities/BotMetaData.java
@@ -1,16 +1,39 @@
 package za.co.entelect.challenge.entities;
 
+import com.google.gson.annotations.SerializedName;
 import za.co.entelect.challenge.enums.BotLanguage;
 
 public class BotMetaData {
+    @SerializedName("author")
+    private String author;
+    @SerializedName("email")
+    private String email;
+    @SerializedName("nickName")
+    private String nickName;
+
+    @SerializedName("botLanguage")
     private BotLanguage botLanguage;
+    @SerializedName("botLocation")
     private String botLocation;
+    @SerializedName("botFileName")
     private String botFileName;
 
     public BotMetaData(BotLanguage language, String botLocation, String botFileName){
         this.botLanguage = language;
         this.botLocation = botLocation;
         this.botFileName = botFileName;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getNickName() {
+        return nickName;
     }
 
     public String getBotLocation(){
